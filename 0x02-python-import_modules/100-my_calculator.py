3a6ba55ad5c8033ecf00b425601a003452c081a1
iamukn <n.u.kingsley@gmail.com>
2022-12-04 20:57:19 +0000

    hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-# Author - IAM_UKN
+# Author IAM_UKN
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
-    i = sys.argv[2]
-    j = '*'
-    if (len(sys.argv) - 1 != 3):
+
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif (i != '/' and i != '-' and i != '+' and i != '*'):
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    elif (i == '/'):
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
-    elif (i == '+'):
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    elif (i == '-'):
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
-    elif (i == j):
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
