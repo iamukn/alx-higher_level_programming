d35e5815ad9b12d83a58f09f036d9de18a448bef
iamukn <n.u.kingsley@gmail.com>
2023-01-13 16:33:32 +0000

    1-square added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#UKN Himself
+
+"""Defining a square """
+
+
+class Square:
+    """ Representing a square """
+
+
+    def __init__(self, size):
+        """ Initializing a size arguament and privatizing it using the 
+        .__size command and assigning it a value of size"""
+
+
+        self.__size = size
