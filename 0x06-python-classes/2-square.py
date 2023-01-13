4894c893da795075218fa43a9642d077d64d82db
iamukn <n.u.kingsley@gmail.com>
2023-01-13 17:15:21 +0000

    hot fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,16 @@
 
 
 class Square:
+    """A module that defines a square"""    
+
     def __init__(self, size=0):
-        """ square defined """
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
