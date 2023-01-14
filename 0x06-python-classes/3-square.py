7405b6ae91dd3f225ea44aae795f07a24af94e6a
iamukn <n.u.kingsley@gmail.com>
2023-01-14 08:41:28 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# UKN_himself
+
+"""defining a square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """ initializing the square class
+        Arg:
+            self: this represents the object
+            size: represents the attribute required
+        Raises: raises an error if size is not a digit
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculating the area
+        Return: Size raised to power 2
+        """"
+
+        return (self.__size ** 2)
