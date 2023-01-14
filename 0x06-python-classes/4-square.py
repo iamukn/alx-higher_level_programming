3dc175904f03e76dc9c65bd04666b53d8e1bfca8
iamukn <n.u.kingsley@gmail.com>
2023-01-14 10:01:28 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,7 +16,10 @@ class Square:
             TypeError: if size is not an interger
             ValueError: if size is less than 0
         """
-
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     @property
