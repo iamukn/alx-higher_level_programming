534e80ceb8582f6963ba5106d31c31e9943fbe6e
iamukn <n.u.kingsley@gmail.com>
2023-01-23 15:29:12 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Ukn himself
+
+""" defining a lookup function"""
+
+
+def lookup(obj):
+    """a lookup method
+    Args:
+        obj: 1st argument
+    """
+
+    return dir(obj)
