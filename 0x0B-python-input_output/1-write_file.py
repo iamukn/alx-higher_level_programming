32f696ad68c7360acb1cfc5e2e74be8649c70228
iamukn <n.u.kingsley@gmail.com>
2023-01-28 23:34:37 +0000

    fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     """Defining the write_file module"""
 
     with open(filename, mode='w', encoding="utf-8") as f:
-        f.write(text)
+         return f.write(text)
