129ec34f6cdc4164c07f1dcb76fd139f0e8f9c7e
iamukn <n.u.kingsley@gmail.com>
2023-01-28 23:30:12 +0000

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# ukn_himself
+import os
+
+"""A module that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Defining the write_file module"""
+
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(text)
