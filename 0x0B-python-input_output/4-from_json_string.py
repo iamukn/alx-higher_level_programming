b088390e958db1a73b5c8e0d5941403551e5d2db
iamukn <n.u.kingsley@gmail.com>
2023-01-29 00:26:04 +0000

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Ukn_himself
+import json
+
+""" defining a json func"""
+
+
+def from_json_string(my_str):
+    """from json module defined"""
+
+    return json.dumps(my_str)
