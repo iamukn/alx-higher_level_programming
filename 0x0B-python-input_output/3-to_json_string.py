c49f59ee8736f13bda996a70105c52b645056999
iamukn <n.u.kingsley@gmail.com>
2023-01-29 00:16:17 +0000

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+""" the Javascript object notation Json"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """defining the json func"""
+
+    return json.dumps(my_obj)
