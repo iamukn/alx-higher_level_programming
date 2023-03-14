13e3743c4f6bd54c287181180c86d1bf6765d351
iamukn <n.u.kingsley@gmail.com>
2023-03-14 23:53:25 +0000

    hot fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i;
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
