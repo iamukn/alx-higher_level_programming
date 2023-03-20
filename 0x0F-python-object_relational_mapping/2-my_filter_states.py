7a4a74662c6005b85a35a87b314c6e061f190306
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:44:11 +0000

    lists all states that start with argv[4] input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Selecting from the database where
+name == argument passed
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    connects to the database if
+    name == main
+    """
+    db = MySQL.connect(host="localhost", user=argv[1],
+                       passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER \
+            BY states.id ASC", [argv[4]])
+
+    rows = cur.fetchall()
+
+    for i in rows:
+        print(i)
