09bae2b0cc19b26ad906ad8a8b84ecb69f8b8a3f
iamukn <n.u.kingsley@gmail.com>
2023-03-20 22:58:12 +0000

    printing the states in a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+db = MySQLdb.connect(host="localhost",
+user=argv[1], password=argv[2],
+db=argv[3], pot=3306)
+
+cur = db.cursor()
+
+state = "SELECT * FROM states"
+
+cur.execute(state)
+
+rows = cur.fetchall()
+
+for i in rows:
+    print(i)
+
+cur.close()
+db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+ORM (MySQL and SQLAlchemy
