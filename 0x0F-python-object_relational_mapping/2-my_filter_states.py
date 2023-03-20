42452ffda94c65c6ee96fa0bf6f3e5f5973cb35d
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:55:59 +0000

    hot fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,13 +13,14 @@ if __name__ == "__main__":
     connects to the database if
     name == main
     """
-    db = MySQLdb.connect(host="localhost", port=3306 user=argv[1],
-                       passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306
+                         user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY \
-            '{}' ORDER BY states.id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states \
+            WHERE name LIKE BINARY '{}' \
+            ORDER BY states.id ASC".format(argv[4]))
 
     rows = cur.fetchall()
 
