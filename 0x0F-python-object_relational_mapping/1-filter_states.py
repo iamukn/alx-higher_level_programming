510876c38c45d2f7a49a624dc36c1e8b893f377c
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:26:09 +0000

    sorted the states that begins with letter N using the WHERE name Like 'N%' query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,6 +9,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ = "__main__":
+    """
+    connects python to
+    MySQL server
+    """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
