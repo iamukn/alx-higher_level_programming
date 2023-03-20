47b0632e5a070077353a1a7eb84260d8885fa258
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:21:34 +0000

    sorted the states that begins with letter N using the WHERE name Like 'N%' query

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Script to list all states that begins
+with letter N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute('USE hbtn_0e_0_usa')
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    rows = cur.fetchall()
+
+    for i in rows:
+        print(i)
