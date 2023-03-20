85e6e3fcada7cbf3f830d24f19050cc087686ad5
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:06:20 +0000

    printing the states in a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,9 +9,8 @@ if __name__ == '__main__':
     from the database.
     """
 
-db = MySQLdb.connect(host="localhost",
-user=argv[1], password=argv[2],
-db=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
 cur = db.cursor()
 
