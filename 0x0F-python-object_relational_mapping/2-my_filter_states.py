6ed62c6c30ffb1b6946bfed3c96d8b544bbf2535
iamukn <n.u.kingsley@gmail.com>
2023-03-20 23:45:21 +0000

    lists all states that start with argv[4] input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER \
-            BY states.id ASC", [argv[4]])
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER \
+            BY states.id ASC".format(argv[4]))
 
     rows = cur.fetchall()
 
