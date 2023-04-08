b73d137fdd0a309bb4d696e47676dea0564c0742
iamukn <n.u.kingsley@gmail.com>
2023-04-08 21:37:07 +0000

    urllib request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     with result as response:
         data = response.read()
         print("Body response:")
-        print("\t- type: ".format(type(data)))
+        print("\t- type: {}".format(type(data)))
         print(f"\t- content: {data}")
-        print("\t- utf8 content: ".format(data.decode("utf8")))
+        print("\t- utf8 content: {}".format(data.decode("utf8")))
