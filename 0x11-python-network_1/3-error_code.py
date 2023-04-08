5aad09f1471761e5c0c550cb15922ed8ab0584af
iamukn <n.u.kingsley@gmail.com>
2023-04-08 23:41:06 +0000

    hot fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-
-import urllib.request
+"""Sends a request to a given URL and displays the response body.
+Usage: ./3-error_code.py <url>
+  - Handles HTTP errors.
+"""
+import sys
 import urllib.error
-from sys import argv
-""" Write a Python script that takes in a URL, sends a
-request to the URL and displays the body of the response
-(decoded in utf-8) Prevents the script from running when its imported """
+import urllib.request
+
 
 if __name__ == "__main__":
+    url = sys.argv[1]
 
-    url = argv[1]
-    req = urllib.request.Request(url)
-    res = urllib.request.urlopen(req)
-        try:
-            with res as f:
-                print(f.read().decode('ascii'))
-        except urllib.error.HTTPError as e:
-            print(f"Error code: {e.code}")
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
