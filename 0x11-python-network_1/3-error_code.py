c13b4f4e090bbed522e798662d69de5bbe996fe1
iamukn <n.u.kingsley@gmail.com>
2023-04-08 23:29:00 +0000

    making a get request and handling the exceptions

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8 """
+
+
+""" Prevents the script from running when its imported """
+import urllib.request
+import urllib.error
+from sys import argv
+if __name__ == "__main__":
+
+    if len(argv) > 1:
+        url = argv[1]
+        try:
+            req = urllib.request.Request(url)
+            res = urllib.request.urlopen(req)
+            with res as f:
+                print(res.read().decode('ascii'))
+        except urllib.error.HTTPError as e:
+            print(f"Error Code: {e.code}")
+
